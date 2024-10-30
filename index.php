2c910ec14f27d40e7de7f69248a450c1743e1ba7
CarlosAlbertoCode <alberto2007s10@gmail.com>
2024-10-30 07:55:40 -0300

    botao cadastro adicionado e css criado

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prova de PW - 30.10</title>
 </head>
 <body>
     <h1> Carlos Alberto Lima Amorim e Eder da Silva Cruz</h1>
     <img src="imagens/pizza.jpg" alt="">
+
+    <a href="cadastros.php"> <button>cadastro</button></a>
+
 </body>
 </html>
